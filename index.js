4c671fedf2df0ea0bfc7f2784277e6a2bef1ef5b
LoggeL <hyper.xjo@gmx.de>
2020-09-10 23:42:50 +0200

    inital commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Plugin } = require('powercord/entities')
+const { findInReactTree } = require('powercord/util')
+const { getModule, React } = require('powercord/webpack')
+const { inject, uninject } = require('powercord/injector')
+
+module.exports = class CopyAvatarUrl extends Plugin {
+    async startPlugin() {
+        const Menu = await getModule(['MenuItem'])
+        const MenuItem = Menu.MenuItem
+        console.log(MenuItem)
+        const MessageContextMenu = await getModule(m => m.default && m.default.displayName == 'MessageContextMenu')
+
+        inject('logge-ddg-search', MessageContextMenu, 'default', (args, res) => {
+            if (!findInReactTree(res, c => c.props && c.props.id == 'logge-ddg-search')) res.props.children.splice(4, 0,
+                React.createElement(MenuItem,
+                    {
+                        action: () => {
+                            let markedText = window.getSelection().toString()
+                            if (!markedText) markedText = args[0].message.content
+                            console.log("Should open", "https://ddg.gg/" + encodeURIComponent(markedText))
+
+                            require("electron").shell.openExternal("https://ddg.gg/" + encodeURIComponent(markedText))
+                        },
+                        id: 'logge-ddg-search',
+                        label: 'Search with DDG'
+                    }
+                )
+            )
+            return res
+        })
+        MessageContextMenu.default.displayName = 'MessageContextMenu'
+    }
+
+    pluginWillUnload() {
+        uninject('logge-ddg-search');
+    }
+};
+
+// Inspired by With Mask and Juby210
