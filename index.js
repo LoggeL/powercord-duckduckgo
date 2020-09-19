5bf11a9b06f6fe2eb0cd07a162356824ad204165
withmask <with.mask@tutanota.com>
2020-09-19 10:19:04 +0100

    Ingore none message targets

--- index.js
+++ index.js
@@ -7,67 +7,76 @@ module.exports = class DuckDuckGoSearch extends Plugin {
   async startPlugin() {
     const Menu = await getModule(["MenuItem"]);
     const { MenuItem, MenuGroup } = Menu;
-    console.log(MenuItem);
+
     const MessageContextMenu = await getModule(
       (m) => m.default && m.default.displayName === "MessageContextMenu"
     );
 
-    inject("logge-ddg-search", MessageContextMenu, "default", (args, res) => {
-      res.props.children = res.props.children.filter(
-        (v) => v.props.children?.[0]?.props?.id != "search-google"
-      );
-
-      if (
-        !findInReactTree(
-          res,
-          (c) => c.props && c.props.id === "logge-ddg-search"
-        )
-      ) {
-        res.props.children.splice(
-          1,
-          0,
-          React.createElement(
-            MenuGroup,
-            null,
-            React.createElement(MenuItem, {
-              action: () => {
-                let markedText = window.getSelection().toString();
-                if (!markedText) {
-                  markedText = args[0].message.content;
-                }
-
-                require("electron").shell.openExternal(
-                  `https://ddg.gg/${encodeURIComponent(markedText)}`
-                );
-              },
-              id: "logge-ddg-search",
-              label: "Search with DDG",
-            })
-          )
+    inject(
+      "logge-ddg-search",
+      MessageContextMenu,
+      "default",
+      /**
+       * @param {[{target: HTMLDivElement}]} arg0
+       **/ ([{ target }], res) => {
+        res.props.children = res.props.children.filter(
+          (v) => v.props.children?.[0]?.props?.id != "search-google"
         );
-      }
 
-      return res;
-    });
+        if (
+          !findInReactTree(
+            res,
+            (c) => c.props && c.props.id === "logge-ddg-search"
+          ) &&
+          target.tagName.toLowerCase() == "div" &&
+          target.classList.contains("markup-2BOw-j") &&
+          target.classList.contains("messageContent-2qWWxC")
+        ) {
+          res.props.children.splice(
+            1,
+            0,
+            React.createElement(
+              MenuGroup,
+              null,
+              React.createElement(MenuItem, {
+                action: () => {
+                  let markedText = window.getSelection().toString();
+                  if (!markedText) {
+                    markedText = args[0].message.content;
+                  }
+
+                  require("electron").shell.openExternal(
+                    `https://ddg.gg/${encodeURIComponent(markedText)}`
+                  );
+                },
+                id: "logge-ddg-search",
+                label: "Search with DDG",
+              })
+            )
+          );
+        }
+
+        return res;
+      }
+    );
 
     MessageContextMenu.default.displayName = "MessageContextMenu";
 
     powercord.api.commands.registerCommand({
-      command: 'ddg',
-      description: 'Sends a ddg link.',
-      usage: '{c} [ ...arguments ]',
+      command: "ddg",
+      description: "Sends a ddg link.",
+      usage: "{c} [ ...arguments ]",
       executor: (args) => ({
         send: true,
-        result: 'https://ddg.gg/' + encodeURIComponent(args.join(' '))
-      })
-    })
-
+        result: "https://ddg.gg/" + encodeURIComponent(args.join(" ")),
+      }),
+    });
   }
 
   pluginWillUnload() {
     uninject("logge-ddg-search");
 
-    powercord.api.commands.unregisterCommand('ddg')
+    powercord.api.commands.unregisterCommand("ddg");
   }
 };
 
